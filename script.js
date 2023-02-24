055e1c174e2dfbc1ede50acf8da9d8bee3fab408
Ishan Gupta <ishan.d.gupta@outlook.com>
2023-02-24 18:08:30 +0530

    index.html - renamed buttons according to standard script.js - added comments and boilerplate event listeners. styles.css - reset to previous height values.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const playPauseBtn = document.querySelector('.play-pause-btn');
-const fullscreenBtn = document.querySelector('.fullscreen-btn');
+// Containers
 const videoContainer = document.querySelector('.video-container');
 const video = document.querySelector('video');
 
-// Play Pause Event listerners
-playPauseBtn.addEventListener('click', togglePlay);
-video.addEventListener('click', togglePlay);
+// Buttons
+const openFilesBtn = document.querySelector('.open-files-btn');
+const playlistBtn = document.querySelector('.play-list-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const rwdBtn = document.querySelector('.rwd-btn');
+const playPauseBtn = document.querySelector('.play-pause-btn');
+const ffdBtn = document.querySelector('.ffd-btn');
+const nextBtn = document.querySelector('.next-btn');
+const subtitlesBtn = document.querySelector('.subtitles-btn');
+const pipBtn = document.querySelector('.pip-btn');
+const fullscreenBtn = document.querySelector('.fullscreen-btn');
+const settings = document.querySelector('.settings-btn');
 
+//-- Event Listeners --//
+
+// Video event listener
+video.addEventListener('play', () => {
+    videoContainer.classList.remove('paused');
+});
+
+video.addEventListener('pause', () => {
+    videoContainer.classList.add('paused');
+});
+
+// Keyboard event listners
+//Play Pause
 document.addEventListener('keydown', e => {
     // Remove focus to prevent double event
     e.preventDefault();
@@ -20,23 +41,61 @@ document.addEventListener('keydown', e => {
     }
 });
 
+//-- Control event Listeners --//
+
+// Open Files event listener
+openFilesBtn.addEventListener('click', ()=>{
+    alert("Open Files!");
+});
+
+// Playlist button event listener
+playlistBtn.addEventListener('click', ()=>{
+    alert("Playlist!");
+});
+
+// Previous Button event listener
+prevBtn.addEventListener('click', ()=>{
+    alert("Previous!");
+});
+
+rwdBtn.addEventListener('click', ()=>{
+    alert("Rewind!");
+});
+
+// Play Pause Button event listerner
+playPauseBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+
 function togglePlay() {
     video.paused ? video.play() : video.pause();
 }
 
-video.addEventListener('play', () => {
-    videoContainer.classList.remove('paused');
+// Fast Forward Button event listener
+ffdBtn.addEventListener('click', ()=>{
+    alert("Fast Forward!");
 });
 
-video.addEventListener('pause', () => {
-    videoContainer.classList.add('paused');
+//Next Button event listener
+nextBtn.addEventListener('click', ()=>{
+    alert("Next!");
 });
 
-//Fullscreen event listeners
-video.addEventListener('play', () => {
-    videoContainer.classList.remove('paused');
+// Subtitle Button event listener
+subtitlesBtn.addEventListener('click', ()=>{
+    alert("Subtitles!");
 });
 
-video.addEventListener('pause', () => {
-    videoContainer.classList.add('paused');
+// PiP Button event listener
+pipBtn.addEventListener('click', ()=>{
+    alert("PiP!");
+});
+
+// Fullscreen Button event listener
+fullscreenBtn.addEventListener('click', ()=>{
+    alert("Fullscreen!");
+});
+
+// Settings Button event listener
+settings.addEventListener('click', ()=>{
+    alert("Settings!");
 });
